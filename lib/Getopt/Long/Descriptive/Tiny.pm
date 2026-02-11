34f134069fc6d2094fc4a088dc0c1f2e0e45eb74
Graham Ollis <plicease@cpan.org>
2026-02-11 16:56:12 +0000

    Initial structure

---
+++ lib/Getopt/Long/Descriptive/Tiny.pm
@@ -0,0 +1,11 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+
+package Getopt::Long::Descriptive::Tiny {
+
+  # ABSTRACT: Getopt::Long, but simpler and more powerful with no dependencies
+}
+
+1;
+
