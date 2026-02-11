58075da3243ec564a82c29bbe9fce30a7115086f
Graham Ollis <plicease@cpan.org>
2026-02-11 17:16:01 +0000

    bones

--- lib/Getopt/Long/Descriptive/Tiny.pm
+++ lib/Getopt/Long/Descriptive/Tiny.pm
@@ -5,4 +5,27 @@ package Getopt::Long::Descriptive::Tiny;
 
 use warnings;
 use 5.040;
+use Exporter qw( import );
 
+our @EXPORT_OK = qw( describe_options );
+
+=head1 FUNCTIONS
+
+=head2 describe_options
+
+=cut
+
+sub describe_options ( $usage_desc, @options ) {
+    my $opt = Getopt::Long::Descriptive::Tiny::Options->new( usage_desc => $usage_desc );
+    return ($opt, $opt->usage);
+}
+
+use experimental qw( class );
+
+class Getopt::Long::Descriptive::Tiny::Options;
+
+field $usage_desc :param :reader;
+
+method usage {
+    return $usage_desc;
+}
--- t/getopt_long_descriptive_tiny.t
+++ t/getopt_long_descriptive_tiny.t
@@ -1,7 +1,19 @@
 use Test2::V0 -no_srand => 1;
-use Getopt::Long::Descriptive::Tiny;
+use Getopt::Long::Descriptive::Tiny qw( describe_options );
 
-ok 1, 'todo';
+subtest 'basic' => sub {
+
+    is(
+        [describe_options '%c %o'],
+        array {
+            item object {
+                prop isa => 'Getopt::Long::Descriptive::Tiny::Options';
+                call usage_desc => '%c %o';
+            };
+            item string '%c %o';
+        },
+    );
+};
 
 done_testing;
 
