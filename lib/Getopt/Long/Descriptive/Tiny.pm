77c597faf462f3e92e29d0be4accdd8a487101e9
Graham Ollis <plicease@cpan.org>
2026-02-11 17:01:58 +0000

    simplify

--- lib/Getopt/Long/Descriptive/Tiny.pm
+++ lib/Getopt/Long/Descriptive/Tiny.pm
@@ -1,11 +1,8 @@
+package Getopt::Long::Descriptive::Tiny;
+
+# ABSTRACT: Getopt::Long, but simpler and more powerful with no dependencies
+# VERSION
+
 use warnings;
-use 5.020;
-use experimental qw( postderef signatures );
-
-package Getopt::Long::Descriptive::Tiny {
-
-  # ABSTRACT: Getopt::Long, but simpler and more powerful with no dependencies
-}
-
-1;
+use 5.040;
 
